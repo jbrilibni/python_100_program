54a6cd928fbd5b65e7874006478a7be544e8a3b1
= <=>
2024-11-07 14:41:35 +0700

    geometri

---
+++ trapesium.py
@@ -0,0 +1,15 @@
+print('='*40)
+print('PROGRAM TRAPESIUM')
+print('='*40)
+
+sisi1 = int(input('sisi sejajar 1\t\t: '))
+sisi2 = int(input('sisi sejajar 2\t\t: '))
+sisi3 = int(input('sisi sejajar 4\t\t: '))
+sisi4 = int(input('sisi sejajar 4\t\t: '))
+t = int(input('tinggi\t\t\t: '))
+
+l = 1/2 * (sisi1 + sisi2) * t
+k = sisi1 + sisi2 + sisi3 + sisi4
+
+print(f'luas\t\t\t: {l}')
+print(f'keliling\t\t\t: {t}')
