5dc48eac7131675d56b1134fb34ae0ab4fe7e8db
= <=>
2024-11-07 14:51:10 +0700

    hitung rata-rata

---
+++ hitungratarata.py
@@ -0,0 +1,10 @@
+print('='*40)
+print('MENGHITUNG RATA - RATA BARANG')
+print('='*40)
+
+barang1 = float(input('masukkan harga barang 1 = '))
+barang2 = float(input('masukkan harga barang 2 = '))
+barang3 = float(input('masukan harga barang 3 = '))
+jumlah = barang1 + barang2 + barang3
+hasil_akhir = jumlah/ 20.000
+print(f'anda berhasil mendapatkan point = {hasil_akhir}')
