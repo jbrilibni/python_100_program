7efe7a3098b8ab6b98ec192b9b9bd31225fec327
= <=>
2024-11-08 08:18:43 +0700

    program segi sepuluh

---
+++ segi_sepuluh.py
@@ -0,0 +1,11 @@
+def segi_sepuluh():
+    print('='*40)
+    print('KELILING SEGI SEPULUH')
+    print('='*40)
+
+    s = int(input('masukkan sisimya: '))
+    l = lambda s : 10 * s
+
+    print(f'keliling : {l(s)}')
+
+segi_sepuluh()
