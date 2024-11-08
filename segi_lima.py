774c95baf6a10e754adcc3bb295be6a4acbd2706
= <=>
2024-11-08 08:02:13 +0700

    segi_lima

---
+++ segi_lima.py
@@ -0,0 +1,11 @@
+def segi_lima():
+    print('='*40)
+    print('KELILING SEGI LIMA')
+    print('='*40)
+
+    s = int(input('masukkan panjang sisinya: '))
+    l = 5 * s
+
+    print(f'keliling : {l}')
+
+segi_lima()
