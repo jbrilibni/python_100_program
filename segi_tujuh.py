26f8df6e88f5fd3eb08ac2f0c1d7091b4defa772
= <=>
2024-11-08 07:41:35 +0700

    program keliling segi tujuh

---
+++ segi_tujuh.py
@@ -0,0 +1,13 @@
+# dibuat oleh : Jibril ibni jubair
+# tanggal dibuat : 8 - 11 - 2024
+
+def segi_tujuh():
+    print('='*40)
+    print('KELILING SEGI TUJUH')
+    print('='*40)
+
+    s = int(input('masukkan panjang sisinya: '))
+    l = lambda s : 7 * s
+    print(f'keliling : {l(s)}')
+
+segi_tujuh()
