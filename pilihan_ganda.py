c5aaad401c6f1fd0edf5cb8beeb61dacb2823a56
= <=>
2024-12-11 15:45:26 +0700

    pilihan ganda

---
+++ pilihan_ganda.py
@@ -0,0 +1,12 @@
+print('siapa goat sesungguhnya?')
+print('''
+      a. Ishowspeed
+      b. ragil jerman
+      c. penaldo
+      d. pessi
+      e. neymar juruju''')
+a=(input('masukan jawaban : '))
+if a == 'b':
+    print ('bener')
+else:
+    print ('salah,jawaban nya adalah b')
