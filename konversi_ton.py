2d136f5e49dcb9a2def3da21a56e575422d48b69
= <=>
2024-12-11 17:39:30 +0700

    konversi ton

---
+++ konversi_ton.py
@@ -0,0 +1,15 @@
+print('='*25)
+print('PROGRAM KONVERSI TON')
+print('='*25)
+
+t = float(input('Masukkan Massa Ton: '))
+g = t * 1000000
+kg = t * 1000
+lb = t * 2204.62
+oz = t * 35274
+
+print('Hasil Konversi:')
+print(g, 'Gram')
+print(kg, 'Kilogram')
+print(lb, 'Pound')
+print(oz, 'Ounce')
