7bfc4f82b7afec04ebff3bc4e5fbbee1bf890c98
= <=>
2024-12-11 15:22:53 +0700

    Program Menentukan

---
+++ mencari_waktu.py
@@ -0,0 +1,8 @@
+print('='*40)
+print('Program Mencari Waktu')
+print('='*40)
+
+s=int(input('masukan Jarak (S)'))
+v=int(input('masukan Kecepatan (v)'))
+t=s/v
+print(f'Waktu yang di butuhkan adalah{t}')
