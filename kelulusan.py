c779312220cfbcf3c3d22c603ea192c9537fcc01
= <=>
2024-12-11 17:27:18 +0700

    program kelulusan

---
+++ kelulusan.py
@@ -0,0 +1,12 @@
+print('''
+=================
+PROGRAM KELULUSAN
+=================
+''')
+
+n = input('Masukkan Nilai :')
+
+if float(n) > 75:
+    print('Status Kelulusan : LULUS')
+else:
+    print('Status Kelulusan : TIDAK LULUS')
